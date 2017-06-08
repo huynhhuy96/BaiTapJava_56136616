ce54ef19df237034348b403a5cfd856a6681afb4
huynhhuy96 <huynhhuy96@gmail.com>
2017-06-08 12:35:46 +0700

    Bai14_CustomLayoutforListView

---
+++ Bai14_CustomLayoutforListView/gen/com/huy56136616/bai14_customlayoutforlistview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.huy56136616.bai14_customlayoutforlistview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
